c0f4003b659be6cb99fb106e30d7a4b50ee7f598
Trung Quân <trquan173@gmail.com>
2024-12-17 18:01:55 +0700

    Enums

--- TypeScript_Basic/app.js
+++ TypeScript_Basic/app.js
@@ -1,6 +1,17 @@
-var num1 = document.querySelector('#num1');
-var num2 = document.querySelector('#num2');
-var btn = document.querySelector('input[type=button]');
-btn.addEventListener('click', function () {
-    console.log(+num1.value + +num2.value);
-});
+/* Wellcome typescript*/
+// const num1 = document.querySelector('#num1') as HTMLInputElement
+// const num2 = document.querySelector('#num2') as HTMLInputElement
+// const btn = document.querySelector('input[type=button]') as HTMLButtonElement
+// btn.addEventListener('click', function(){
+//     console.log(+num1.value  + +num2.value)
+// })
+/* Enum */
+var MOVE;
+(function (MOVE) {
+    MOVE[MOVE["Up"] = 1] = "Up";
+    MOVE[MOVE["Down"] = 2] = "Down";
+    MOVE[MOVE["Left"] = 7] = "Left";
+    MOVE[MOVE["Right"] = 8] = "Right";
+})(MOVE || (MOVE = {}));
+var a = MOVE.Down + 2;
+console.log(a + '');
--- TypeScript_Basic/app.ts
+++ TypeScript_Basic/app.ts
@@ -1,8 +1,21 @@
-const num1 = document.querySelector('#num1') as HTMLInputElement
-const num2 = document.querySelector('#num2') as HTMLInputElement
+/* Wellcome typescript*/
+// const num1 = document.querySelector('#num1') as HTMLInputElement
+// const num2 = document.querySelector('#num2') as HTMLInputElement
 
-const btn = document.querySelector('input[type=button]') as HTMLButtonElement
+// const btn = document.querySelector('input[type=button]') as HTMLButtonElement
 
-btn.addEventListener('click', function(){
-    console.log(+num1.value  + +num2.value)
-})
+// btn.addEventListener('click', function(){
+//     console.log(+num1.value  + +num2.value)
+// })
+
+/* Enum */
+enum MOVE {
+    Up,
+    Down, 
+    Left,
+    Right
+}
+
+let a = MOVE.Down + 2
+
+console.log(a + '')
