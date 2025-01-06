ea00c332ee14ff37d7f88a2a80a19e9139b6eb1d
Trung Quân <trquan173@gmail.com>
2025-01-06 17:58:01 +0700

    Create callback function

--- TypeScript_Basic/app.js
+++ TypeScript_Basic/app.js
@@ -1,17 +1,8 @@
 "use strict";
-/* Wellcome typescript*/
-// const num1 = document.querySelector('#num1') as HTMLInputElement
-// const num2 = document.querySelector('#num2') as HTMLInputElement
-// const btn = document.querySelector('input[type=button]') as HTMLButtonElement
-// btn.addEventListener('click', function(){
-//     console.log(+num1.value  + +num2.value)
-// })
-/* Enum */
-var MOVE;
-(function (MOVE) {
-    MOVE[MOVE["Up"] = 0] = "Up";
-    MOVE[MOVE["Down"] = 1] = "Down";
-    MOVE[MOVE["Left"] = 2] = "Left";
-    MOVE[MOVE["Right"] = 3] = "Right";
-})(MOVE || (MOVE = {}));
-console.log(MOVE);
+const addHandle = (num1, num2, cb) => {
+    num1 *= 10;
+    num2 *= 100;
+    return cb(num1 + num2);
+};
+const pText = document.getElementById('result');
+pText.innerText = addHandle(3, 5, (result) => result) + '';
