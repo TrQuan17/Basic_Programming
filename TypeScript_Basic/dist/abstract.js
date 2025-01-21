9981ccd8ac2439edbd017b7d96c8ee0d7990314e
Trung Quân <trquan173@gmail.com>
2025-01-22 01:20:48 +0700

    Update abstract

--- TypeScript_Basic/dist/abstract.js
+++ TypeScript_Basic/dist/abstract.js
@@ -1,4 +1,30 @@
 "use strict";
 class Department {
+    constructor(id) {
+        this.id = id;
+    }
 }
+class ITDepartment extends Department {
+    constructor(id, name = 'IT Department', employees = []) {
+        super(id);
+        this.name = name;
+        this.employees = employees;
+    }
+    addEmployees(employee) {
+        if (!employee.language) {
+            throw new Error(`Not eligible to join ${this.name}`);
+        }
+        this.employees.push(employee);
+    }
+    get employeesList() {
+        return this.employees;
+    }
+}
+const IT = new ITDepartment('1');
+const employee = {
+    name: 'TrQuan',
+    language: 'Typescript'
+};
+IT.addEmployees(employee);
+console.log(IT.employeesList);
 //# sourceMappingURL=abstract.js.map
--- TypeScript_Basic/src/abstract.ts
+++ TypeScript_Basic/src/abstract.ts
@@ -1,4 +1,45 @@
-abstract class Department {
-    abstract addEmployees():void
+type Employee = {
+    name: string,
+    language: string
 }
 
+abstract class Department {
+
+    abstract name: string
+
+    constructor(protected id: string) { }
+
+    abstract addEmployees(employee: Employee): void
+}
+
+class ITDepartment extends Department {
+    constructor(
+        id: string,
+        public name: string = 'IT Department',
+        private employees: Employee[] = []
+    ) {
+        super(id)
+    }
+
+    addEmployees(employee: Employee): void {
+        if (!employee.language) {
+            throw new Error(`Not eligible to join ${this.name}`)
+        }
+
+        this.employees.push(employee)
+    }
+
+    get employeesList() {
+        return this.employees
+    }
+}
+
+const IT = new ITDepartment('1')
+
+const employee: Employee = {
+    name: 'TrQuan',
+    language: 'Typescript'
+}
+IT.addEmployees(employee)
+
+console.log(IT.employeesList)
