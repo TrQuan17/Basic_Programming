84585e15ed390886506a001adbc7fcadcb46b6a8
Trung Quân <trquan173@gmail.com>
2025-01-23 17:35:11 +0700

    Interface

---
+++ TypeScript_Basic/dist/singletons_pattern.js
@@ -0,0 +1,11 @@
+class History {
+    static instance(id, log, type) {
+    }
+    constructor(id, log, type) {
+        this.id = id;
+        this.log = log;
+        this.type = type;
+    }
+}
+export {};
+//# sourceMappingURL=singletons_pattern.js.map
